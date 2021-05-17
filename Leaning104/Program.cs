f0b6f382685539e607e606e88ce879fc936ac87c
AiyEm55 <souksavat.am@gmail.com>
2021-05-17 09:55:27 +0700

    Add project files.

---
+++ Leaning104/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Leaning104
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
